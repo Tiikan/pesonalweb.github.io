976cf273120538df7e043f6df6041acd2e928155
Ngan kang <144213614+Tiikan@users.noreply.github.com>
2023-09-06 16:59:06 +0700

    Add files via upload

---
+++ link.js
@@ -0,0 +1,59 @@
+let menuIcon =document.querySelector('#menu-icon');
+let navber =document.querySelector('.navber');
+
+menuIcon.onclick =()=>{
+    menuIcon.classList.toggle('bxs-x-circle');
+    navber.classList.toggle('active');
+};
+
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () =>{
+    sections.forEach(sec =>{
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top <offset + height){
+            navLinks.forEach(links =>{
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+
+    let header = document.querySelector('header');
+
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    
+
+    menuIcon.classList.remove('bxs-x-circle');
+    navber.classList.remove('active');
+};
+
+
+ScrollReveal({ 
+     reset: true,
+   distance: '80px',
+   duration: 2000,
+   delay:200
+});
+
+ScrollReveal().reveal('.home-comtent, .heading ,.about-img2', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box , .contact form', { origin: 'bottom' });
+ScrollReveal().reveal(' .home-comten h1, .about-img', { origin: 'left' });
+
+ScrollReveal().reveal(' .home-comten p, .about-content ', { origin: 'right' });
+/* typed js */
+const typed =new Typed('.multilpe-text',{
+   strings: [' MATHEMATICS FOR COMPUTER  ','C++ PROGRAMMING II','WRITING SKILLS ENGLISH',
+'WEB PAGE DYNAMICS' ,'WEB-BASED DEVELOPMENT I'],
+   typeSpeed: 100,
+   backSpeed: 100,
+   backDelay: 1000,
+   loop: true
+});
